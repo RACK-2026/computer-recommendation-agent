c4fe0af7bc88f230d9f87c4ee9776ae1b202ee34
RACK-2026 <2796690649@qq.com>
2026-08-28 17:09:28 +0800

    Add app/models/__init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,25 @@
+"""枚举定义"""
+from enum import Enum
+
+
+class DeviceType(str, Enum):
+    LAPTOP = "笔记本"
+    DESKTOP = "台式机"
+    BOTH = "都推荐"
+
+
+class UserGroup(str, Enum):
+    STUDENT = "学生"
+    GAMER = "游戏玩家"
+    BUSINESS = "商务办公"
+    DESIGNER = "设计师/程序员"
+    GENERAL = "普通家用"
+
+
+class ConversationStep(str, Enum):
+    GREETING = "greeting"
+    CLARIFYING = "clarifying"
+    SEARCHING = "searching"
+    RECOMMENDING = "recommending"
+    FOLLOWUP = "followup"
+
